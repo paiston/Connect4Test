9b77afa6b621ecf818c59a1889399e68391186c8
paiston <aiston.phil@googlemail.com>
2015-10-06 22:45:46 +0100

    removed computer vs computer

--- Connect4.Console/Program.cs
+++ Connect4.Console/Program.cs
@@ -18,15 +18,9 @@ namespace Connect4.Console
             BoardSettings boardSettings = new BoardSettings() { Columns = 7, Rows = 6, WinningCount = 4, Turn = 1, MoveCount = 0 };
             
             Connect4 c4 = new Connect4(boardSettings, new Message());
-            c4.Player1 = new Human();
-            c4.Player2 = new Human();
-            //c4.Player2 = new Computer(2, boardSettings);
-
-            //Uncomment code below for Computer vs computer game.
-            //c4.Player1 = new Computer(1, boardSettings);
-            //(c4.Player1 as Computer).CutOffLevel = 7;
-            //(c4.Player2 as Computer).Weights = new int[] { 1, 2, 4, 8, 1, 2, 8, 16 };
-
+            c4.Player1 = new Human(); 
+            c4.Player2 = new Computer(2, boardSettings);
+                        
             c4.PlayGame();
 
             System.Console.Write("Would you like another game? (Y or N)");
